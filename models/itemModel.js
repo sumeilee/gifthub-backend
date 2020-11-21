5bf599458289f0b212f8c0338ce3a38bfccae8d7
cw <illumea@gmail.com>
2020-11-21 14:17:14 +0800

    updated item model

---
+++ models/itemModel.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+    // var name below TBC
+    postID: {
+        type: Number,
+        required: true,
+    },
+    postType: {
+        type: String,
+        enum: ["request", "offer"],
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        max: 200,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    images: [String],
+    delivery: {
+        type: String,
+        enum: ["Included", "Not included"],
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["Open", "Pending Fulfilment", "Fulfilled"],
+        required: true,
+    },
+    tags: [String],
+    postedBy: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+});
+
+const ItemModel = mongoose.model("Item", itemSchema);
+
+module.exports = ItemModel;
