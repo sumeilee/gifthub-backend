38362da621a7650f069c41c794cc24b5f4ceb7be
cw <illumea@gmail.com>
2020-11-30 02:22:08 +0800

    add transaction seed data

---
+++ scripts/seed/transactionData.js
@@ -0,0 +1,33 @@
+const transactions = [
+    // {
+    //     item: "5fbbdfb0cf922462189986f6",
+    //     requestorID: "5fb8b003e1e86a126a37520d",
+    //     donorID: "5fb8d9e0af77ce1b35be26d5",
+    // },
+    // {
+    //     item: "5fbbdfb0cf922462189986f9",
+    //     requestorID: "5fb8da0aaf77ce1b35be26d6",
+    //     donorID: "5fb8b003e1e86a126a37520d",
+    // },
+    {
+        //no convo yet
+        //check usertypes in items db
+        item: "5fbbdfb0cf922462189986fa",
+        requestorID: "5fb8d9e0af77ce1b35be26d5",
+        donorID: "5fb8da5caf77ce1b35be26d9",
+    },
+    {
+        //no convo yet
+        item: "5fbbe06a8458d16220c23fef",
+        requestorID: "5fbe6c40ae456c29c546cda5",
+        donorID: "5fbe68c5ae456c29c546cda3",
+    },
+    {
+        //no convo yet
+        item: "5fbbdfb0cf922462189986f7",
+        requestorID: "5fb8da0aaf77ce1b35be26d6",
+        donorID: "5fb8b003e1e86a126a37520d",
+    },
+];
+
+module.exports = transactions;
